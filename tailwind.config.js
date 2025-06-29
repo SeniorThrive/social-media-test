d44f8952bf2ca9d8059e73d265b9c6aed218b689
SeniorThrive <162615759+SeniorThrive@users.noreply.github.com>
2025-06-29 12:39:27 -0700

    Fix Tailwind ring color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,6 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    ringColor: ({ theme }) => ({
-      ...theme('colors'),
-    }),
-    ringOffsetColor: ({ theme }) => ({
-      ...theme('colors'),
-    }),
     extend: {
       colors: {
         st_light_blue: '#3762D4',
