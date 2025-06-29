80cef34eaec10823577b775b43e5deda5a0b8e84
SeniorThrive <162615759+SeniorThrive@users.noreply.github.com>
2025-06-29 08:57:32 -0700

    Fix Supabase Configuration

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = "https://bjhhahxhagtiqfdcxzyr.supabase.co";
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+if (!supabaseURL || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
